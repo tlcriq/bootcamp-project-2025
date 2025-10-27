6914147f22293dd3739583a822c94afb49db8362
tlcriq <tatecriqui@gmail.com>
2025-10-27 10:52:41 -0700

    work on porting over html pages

---
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    [
+        {
+            title: "Initial testing entry",
+            date: "October 10th, 2025",
+            description: "This is my blog. This is an example post, so here's a picture of how the site looked a couple days ago.",
+            image: "this_website.png",
+            imageAlt: "Just the html here",
+            slug: "first-entry"
+        },
+        {
+            title: "Second testing entry",
+            date: "October 10th, 2025",
+            description: "I am creating a typescript function to make these interactable. Here's how the site looks now.",
+            image: "website_1.1.png",
+            imageAlt: "second page version",
+            slug: "second-entry"
+        }
+    ]
+  )
+}
--- bootcamp-milestone-2/src/app/page.tsx
+++ bootcamp-milestone-2/src/app/page.tsx
@@ -1,9 +1,62 @@
-
+import Image from "next"
 
 export default function Home() {
   return (
-    <div>
-      //stuff for later
-    </div>
+<html>
+    <head>
+        <title>TateSite</title>
+        <link rel="stylesheet" href="styles.css" />
+    </head>
+
+    <body>
+
+        <main>
+            <h1 className = "page-title">
+                Tate Criqui
+            </h1>
+
+            <div className="about">
+                <div className="about-image">
+                    <img src = "Tate2.jpeg" alt = "It's me!" height = {300} />
+                </div>
+                <div className="about-text">
+                    <p> 
+                        <strong>Tate Criqui</strong> is a freshman computer science major
+                        from San Diego, CA.
+                    </p>
+                    
+                    <p> 
+                        Previously, he has worked with <strong>The League of Amazing Programmers</strong>
+                        (<em>a nonprofit organization for young coders</em>), teaching classes on 
+                        <strong>Java</strong> and <strong>Python</strong> programming.
+                    </p>
+                        
+                    <p>
+                        He has also created several large-scale projects, such as an <strong>action-adventure game</strong>,
+                         a <strong>Discord bot</strong>, and a <strong>motion detection system</strong> using <strong>computer vision</strong>.
+                        For his high school <strong>capstone engineering project</strong>, he collaborated with 
+                        <strong>NexStream Technical Education</strong> to create a curriculum for
+                        a <strong>digital electronics college class</strong>.
+                    </p>
+
+                    <p>
+                        Outside of his work and studies, Tate also enjoys <strong>hiking, reading, and acting</strong>. 
+                        <br/> In high school, he featured in several plays, and was at different times 
+                        a starring role, stage manager/lighting technician, and assistant director.
+                        He also wrote and staged an <strong>original whodunnit comedic play</strong>.
+                        <br/> He additionally took up a <strong>leadership role</strong> in several clubs, guiding fellow students through 
+                        improv comedy, classical guitar, and dramatic speech.
+                        <br/> Plus, he's fun to be around.
+                        <br/> Or so he's told.
+                    </p>
+                </div>
+            </div>
+        </main>
+
+        <footer className="footer">
+            © 2023 TateSite | All Rights Reserved
+        </footer>
+    </body>
+</html>
   )
 }
